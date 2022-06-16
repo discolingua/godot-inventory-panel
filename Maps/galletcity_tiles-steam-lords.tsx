3cada1688ed1f6fbc6759e2065f31b076126fa21
Bunny Blake <discolingua@gmail.com>
2022-06-16 01:15:57 -0400

    tilemap import works, had to edit image paths in the tmx/tsx files generated by Tiled.

---
+++ Maps/galletcity_tiles-steam-lords.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="galletcity_tiles-steam-lords" tilewidth="8" tileheight="8" tilecount="168" columns="8">
+ <image source="res://Maps/galletcity_tiles-steam-lords.png" width="64" height="168"/>
+</tileset>
